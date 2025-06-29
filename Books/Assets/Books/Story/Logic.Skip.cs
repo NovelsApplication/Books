ad44064ca3828d6210ba961797e3e98a11da23b2
Ivan A. <Ivan@Antonishin.com>
2025-06-29 14:55:51 +0300

    add skip tags

--- Books/Assets/Books/Story/Logic.Skip.cs
+++ Books/Assets/Books/Story/Logic.Skip.cs
@@ -4,7 +4,7 @@ namespace Books.Story
 {
     internal partial class Logic
     {
-        [Logic(LogicIdx.Название, LogicIdx.Жанры, LogicIdx.Бирка, LogicIdx.Раздел, LogicIdx.Постер, LogicIdx.Аннотация)]
+        [Logic(LogicIdx.Название, LogicIdx.Жанры, LogicIdx.Бирка, LogicIdx.Раздел, LogicIdx.Постер, LogicIdx.Аннотация, LogicIdx.Серия)]
         private async UniTask<bool> RunSkip(string header, string attributes, string body)
         {
             await UniTask.NextFrame();
