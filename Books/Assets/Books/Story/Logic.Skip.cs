0f03c2e95f49cd487a2a40b241ab0f15d2ec8a94
Ivan A. <Ivan@Antonishin.com>
2025-06-29 14:53:32 +0300

    add skip logic

---
+++ Books/Assets/Books/Story/Logic.Skip.cs
@@ -0,0 +1,14 @@
+using Cysharp.Threading.Tasks;
+
+namespace Books.Story
+{
+    internal partial class Logic
+    {
+        [Logic(LogicIdx.Название, LogicIdx.Жанры, LogicIdx.Бирка, LogicIdx.Раздел, LogicIdx.Постер, LogicIdx.Аннотация)]
+        private async UniTask<bool> RunSkip(string header, string attributes, string body)
+        {
+            await UniTask.NextFrame();
+            return true;
+        }
+    }
+}
