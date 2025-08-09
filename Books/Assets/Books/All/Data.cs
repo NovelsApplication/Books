335641d38878de5c1505742703b351723bc8f917
Ivan A. <Ivan@Antonishin.com>
2025-08-09 10:50:54 +0300

    fix some nre

--- Books/Assets/Books/All/Data.cs
+++ Books/Assets/Books/All/Data.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-namespace Books 
+namespace Books.All
 {
     [Serializable]
     internal struct Data
--- Books/Assets/Books/All/EntryPoint.cs
+++ Books/Assets/Books/All/EntryPoint.cs
@@ -2,7 +2,7 @@ using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.LowLevel;
 
-namespace Books
+namespace Books.All
 {
     internal sealed class EntryPoint : MonoBehaviour
     {
