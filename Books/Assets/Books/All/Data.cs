cf16631618468bd8ce87af6c77d9a38095874c9a
Ivan A. <Ivan@Antonishin.com>
2025-08-12 22:21:05 +0300

    update

--- Books/Assets/Books/All/Data.cs
+++ Books/Assets/Books/All/Data.cs
@@ -6,10 +6,12 @@ namespace Books.All
     [Serializable]
     internal struct Data
     {
+        [SerializeField] private string _testURL;
         [SerializeField] private Loading.Data _loadingData;
         [SerializeField] private Menu.Data _menuData;
         [SerializeField] private Story.Data _storyData;
 
+        public readonly string TestURL => _testURL;
         public readonly Loading.Data LoadingData => _loadingData;
         public readonly Menu.Data MenuData => _menuData;
         public readonly Story.Data StoriesData => _storyData;
