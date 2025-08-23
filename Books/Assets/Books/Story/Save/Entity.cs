43e30220dcbb5a6533ee95614a3158849d21548e
Ivan A. <Ivan@Antonishin.com>
2025-08-23 21:11:34 +0300

    DOTween fix

---
+++ Books/Assets/Books/Story/Save/Entity.cs
@@ -0,0 +1,15 @@
+using Books.Story.View;
+using Cysharp.Threading.Tasks;
+using Shared.Disposable;
+using System;
+using UniRx;
+using UnityEngine;
+
+namespace Books.Story.Save 
+{
+    public class Entity : BaseDisposable
+    {
+
+    }
+}
+
