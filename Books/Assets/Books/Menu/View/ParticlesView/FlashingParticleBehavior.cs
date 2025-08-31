5c93da66805d02b3556355b20fe6653896ce43d6
Mm <maxkulezenin@gmail.com>
2025-08-31 15:24:23 +0700

    refactoring

--- Books/Assets/Books/Menu/View/ParticlesView/FlashingParticleBehavior.cs
+++ Books/Assets/Books/Menu/View/ParticlesView/FlashingParticleBehavior.cs
@@ -14,31 +14,27 @@ namespace Books.Menu.View.ParticlesView
         [Range(0, 1)] [SerializeField] private float _minFadeValue;
 
         [Header("Продолжительность итерации мигания")] 
-        [Range(1, 5)] [SerializeField] private float _flashCycleDuration;
+        [Range(1, 5)] [SerializeField] private float _flashCycleDuration = 1f;
 
         private Sequence _flashingSequence;
 
         public override void ActivateAnimation(Action callback = null)
         {
             base.ActivateAnimation(callback);
-            Sequence.Pause();
-
-            _flashingSequence.Kill();
+            
+            CleanupFlashingSequence();
             _flashingSequence = DOTween.Sequence();
             _flashingSequence.Pause();
 
-            if (_useStartFadeAsMax)
-                _maxFadeValue = startFade;
-            
+            if (_useStartFadeAsMax) _maxFadeValue = startFade;
             _minFadeValue = Mathf.Clamp(_minFadeValue, _minFadeValue, _maxFadeValue);
             
             float flashingDuration = duration - duration * fadeInPercentage - duration * fadeOutPercentage;
-            if (flashingDuration == 0)
+            
+            if (flashingDuration <= 0)
             {
-                Debug.Log($"Необходимо снизить долю времени появления и угасания. Прололжительность мерцания равна 0 у объекта {gameObject.name}");
-                
-                Sequence.Play();
-                
+                Debug.LogWarning($"Необходимо снизить долю времени появления и угасания. Продолжительность мерцания равна {flashingDuration} у объекта {gameObject.name}");
+                MainSequence.Play();
                 return;
             }
 
@@ -46,23 +42,30 @@ namespace Books.Menu.View.ParticlesView
             
             for (int i = 0; i < cycles; i++)
             {
-                _flashingSequence.Append(Image.DOFade(_minFadeValue, _flashCycleDuration / 2)).SetEase(Ease.InOutQuad);
-                _flashingSequence.Append(Image.DOFade(_maxFadeValue, _flashCycleDuration / 2)).SetEase(Ease.InOutQuad);
+                _flashingSequence.Append(Image.DOFade(_minFadeValue, _flashCycleDuration / 2).SetEase(Ease.InOutQuad));
+                _flashingSequence.Append(Image.DOFade(_maxFadeValue, _flashCycleDuration / 2).SetEase(Ease.InOutQuad));
             }
 
-            Sequence.Insert(duration * fadeInPercentage, _flashingSequence);
-            Sequence.Play();
+            MainSequence.Insert(duration * fadeInPercentage, _flashingSequence);
+            MainSequence.Play();
         }
         
         private void OnEnable()
         {
-            if (_flashingSequence != null)
-            {
-                _flashingSequence.Play();
-            }
+            _flashingSequence?.Play();
         }
 
         private void OnDisable()
+        {
+            CleanupFlashingSequence();
+        }
+
+        private void OnDestroy()
+        {
+            CleanupFlashingSequence();
+        }
+
+        private void CleanupFlashingSequence()
         {
             if (_flashingSequence != null)
             {
