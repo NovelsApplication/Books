06dc28d11174116c37d63d796cac560b9d21a831
Mm <maxkulezenin@gmail.com>
2025-09-22 14:36:04 +0700

    layout fixes

--- Books/Assets/Books/Menu/View/Screen.cs
+++ Books/Assets/Books/Menu/View/Screen.cs
@@ -136,7 +136,7 @@ namespace Books.Menu.View
             screenLittleBook.gameObject.SetActive(true);
             screenLittleBook.SetLabels(label);
             screenLittleBook.SetImage(poster);
-            screenLittleBook.SetMainTags(new [] {Entity.MainTags.All, Entity.MainTags.InProgress, Entity.MainTags.Continue, Entity.MainTags.Free});
+            screenLittleBook.SetMainTags(mainTags);
             screenLittleBook.SetButton(() =>
             {
                 OpenScreenBookPopUp(poster, storyHeader, description, onClick);
