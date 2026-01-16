11e3f930c2610259d24fe644ff13d4ff01f839c1
Mm <maxkulezenin@gmail.com>
2026-01-17 04:20:49 +0700

    main menu animation

---
+++ Books/Assets/Books/Wardrobe/View/Menu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Books.Wardrobe.View
+{
+    public class Menu : MonoBehaviour
+    {
+        [SerializeField] private Button _openButton;
+        [SerializeField] private Button _closeButton;
+        [SerializeField] private CanvasGroup _topCanvasGroup;
+        [SerializeField] private OpenClose_Animation _animation;
+
+        private void Start()
+        {
+            _openButton.onClick.AddListener(Open);
+            _closeButton.onClick.AddListener(Close);
+        }
+
+        public async void Open()
+        {
+            _openButton.gameObject.SetActive(false);
+            _topCanvasGroup.alpha = 1;
+            
+            await _animation.Open();
+            
+            _closeButton.gameObject.SetActive(true);
+        }
+
+        public async void Close()
+        {
+            _closeButton.gameObject.SetActive(false);
+            _topCanvasGroup.alpha = 0;
+            
+            await _animation.Close();
+
+            _openButton.gameObject.SetActive(true);
+        }
+    }
+}
